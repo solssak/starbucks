ea21e73ada1d728271ad84d82c109c1a962f4f5e
solssak <gswoong1012@gmail.com>
2022-06-14 00:11:37 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 
 const badgeEl = document.querySelector('header .badges');
@@ -52,14 +36,14 @@ toTopEl.addEventListener('click', function () {
   gsap.to(window, .7, {
     scrollTo: 0
   });
-})
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
     delay: (index + 1) * .7,
     opacity: 1
-  })
+  });
 });
 
 // new Swiper(선택자, 옵션)
@@ -67,7 +51,7 @@ new Swiper('.notice-line .swiper-container', {
   direction: 'vertical',
   autoplay: true,
   loop: true
-})
+});
 
 new Swiper('.promotion .swiper-container', {
   slidesPerView: 3,   // 한번에 보여줄 슬라이드 개수
@@ -133,6 +117,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
 })
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2022
